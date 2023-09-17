cb8b567216bb423b7be09ea139348308ac098ee5
Abhishek Bhattacharya <abhishek.bhattacharya0209@gmail.com>
2023-09-17 16:26:09 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,26 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Hover effect for project sections
+document.querySelectorAll('.project').forEach(project => {
+  project.addEventListener('mouseover', function() {
+      this.style.transform = 'scale(1.1)';
+  });
+
+  project.addEventListener('mouseout', function() {
+      this.style.transform = 'scale(1)';
+  });
+});
+
